65ae7e1f57fffe781ef44223e0ac623e1c26cf4f
xsjark <xsjark@protonmail.com>
2021-11-23 10:37:03 -0500

    snipcart

--- src/components/post-body.js
+++ src/components/post-body.js
@@ -26,7 +26,7 @@ export default function PostBody({ content, title, coverImage, date, price, slug
         <button id={title} class="snipcart-add-item bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
           data-item-id={title}
           data-item-price={price}
-          data-item-url={"https://selvaverdetours.gatsbyjs.io/posts/" + slug}
+          data-item-url={"https://selvaverdetours.gatsbyjs.io/posts/" + slug + "/#"} 
           data-item-description={date}
           data-item-image={coverImage?.gatsbyImageData}
           data-item-name={title}
