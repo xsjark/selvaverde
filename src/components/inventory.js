cde4b6a7ac03cab64be99233438f55411424b305
xsjark <xsjark@protonmail.com>
2021-11-24 12:07:50 -0500

    api

--- src/components/inventory.js
+++ src/components/inventory.js
@@ -1,23 +1,27 @@
 import React, { useState, useEffect }  from 'react'
 
 function Inventory() {
-    const [starsCount, setStarsCount] = useState(0)
-  useEffect(() => {
-    // get data from GitHub api
-    fetch(`https://app.snipcart.com/api/products`, {
-        headers: {
-            'Authorization': `Basic ${btoa('Mjk4NmY4YTQtYzEyNi00MDc1LWEyZDEtMWU3YjIzNzUyMzQ2NjM3NzMyNzQ0NDM4MzA4Mjk3')}`,
-            'Accept': 'application/json'
-        }
-     })
-      .then(response => response.json()) // parse JSON from request
-      .then(resultData => {
-        setStarsCount(resultData)
-      }) // set data for the number of stars
-  }, [])
+    
+
+const [data, setData] = useState(null);
+useEffect(() => {
+    async function loadData() {
+        const response = await fetch('https://app.snipcart.com/api/orders', {
+            headers: {
+                'Authorization': `Basic ${btoa('Mjk4NmY4YTQtYzEyNi00MDc1LWEyZDEtMWU3YjIzNzUyMzQ2NjM3NzMyNzQ0NDM4MzA4Mjk3')}`,
+                'Accept': 'application/json'
+            }})
+
+        const posts = await response.json();
+        setData(posts);
+    }
+
+    loadData();
+}, []);
+
     return (
         <div>
-            <p>{starsCount}</p>
+            <p>{data}</p>
         </div>
     )
 }
