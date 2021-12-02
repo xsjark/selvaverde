165bf4c5352770618cbe4bfaa54448842a98ff63
xsjark <xsjark@protonmail.com>
2021-12-01 19:45:36 -0500

    logo added

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,12 +1,20 @@
 import React from "react";
+import logo from "../assets/logo.jpg" 
 
 export default function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Selva Verde.
-      </h1>
-      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
+      <div class="flex mb-4 content-center">
+  <div class="w-1/4 h-48">
+    <img className="object-contain h-48 w-48" src={logo} alt="Logo" />
+  </div>
+  <span class="w-3/4 h-24 self-center">
+    <h1 className="text-4xl md:text-7xl font-bold tracking-tighter leading-tight md:pr-8 ">
+        Selva Verde
+    </h1>
+  </span>
+  </div>
+      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8 ">
         Jungle, rafting and community tours in the Amazon
       </h4>
     </section>
