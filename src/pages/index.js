9ce804164f69ba9e205d4fa082e1087ad1e4402b
xsjark <xsjark@protonmail.com>
2021-12-16 13:03:04 -0500

    fixed disappearing shopping cart on mobile and fixed irregular hero image width

--- src/pages/index.js
+++ src/pages/index.js
@@ -47,10 +47,10 @@ export default function Index({ data: { allPosts, site, blog } }) {
               </a>
             </li>
           </ul>
-          <button className="flex justify-end p-4 md:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
-            </svg>
+          <button className="flex snipcart-summary justify-end p-4 md:hidden">
+          <a href="#" className="flex snipcart-checkout items-center px-4 -mb-1 border-b-2 border-transparent">
+              <ShoppingBagIcon className="h-7 w-7 text-gray-500"/>
+              </a>
           </button>
         </div>
       </header>
